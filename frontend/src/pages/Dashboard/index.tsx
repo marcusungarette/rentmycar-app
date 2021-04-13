70dd114cdc54572a3213c2c2b5a747dbc61ce2a2
marcusungarette <marcusungarette@gmail.com>
2021-04-13 12:04:31 -0300

    Dashboard page getting user cars information from api

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,3 +1,41 @@
+import { useEffect, useState } from 'react';
+import api from '../../services/api';
+
+interface Cars {
+  _id: number;
+  thumbnail: string;
+  model: string;
+  category: string;
+  brand: string;
+  price: number;
+  options: [string];
+}
+
 export default function Dashboard() {
-  return <div></div>;
+  const [cars, setCars] = useState<Cars[]>([]);
+
+  useEffect(() => {
+    async function loadCars() {
+      const user_id = localStorage.getItem('user');
+      const response = await api.get('/dashboard', {
+        headers: { user_id },
+      });
+
+      setCars(response.data);
+    }
+    loadCars();
+  }, []);
+  return (
+    <>
+      <ul>
+        {cars.map(car => (
+          <li key={car._id}>
+            <header />
+            <strong>{car.category}</strong>
+            <span>{car.price}</span>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
