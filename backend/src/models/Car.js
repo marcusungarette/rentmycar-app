4a80a522662712f03251c07de9ebd03dc35d4c81
marcusungarette <marcusungarette@gmail.com>
2021-04-13 12:10:43 -0300

    Generate thumbnail url using virtual json

--- backend/src/models/Car.js
+++ backend/src/models/Car.js
@@ -1,16 +1,28 @@
 const mongoose = require('mongoose');
 
-const CarSchema = new mongoose.Schema({
-  thumbnail: String,
-  model: String,
-  category: String,
-  brand: String,
-  price: Number,
-  options: [String],
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
+const CarSchema = new mongoose.Schema(
+  {
+    thumbnail: String,
+    model: String,
+    category: String,
+    brand: String,
+    price: Number,
+    options: [String],
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
   },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// Virtuals generate thumbnail url
+CarSchema.virtual('thumbnail_url').get(function () {
+  return `http://localhost:3333/files/${this.thumbnail}`;
 });
 
 module.exports = mongoose.model('Car', CarSchema);
